b1f7d417b982dfb85adc5f3095f182a5e57b9e92
Bona <bnvt15@gmail.com>
2023-05-24 15:34:46 +0700

    new2

--- B.php
+++ B.php
@@ -1,20 +1,14 @@
 <?php
-// Mendapatkan data yang dikirimkan melalui metode POST
-$name = $_POST['name'];
-$email = $_POST['email'];
-
-// Mengambil angka terakhir dari email (misalnya, jika email adalah "user1234@example.com", maka angka terakhirnya adalah 1234)
-$lastNumber = intval(substr(strrchr($email, '@'), 1));
-
-// Membuat array dengan jumlah elemen sesuai dengan angka terakhir dari email
-$data = array();
-for ($i = 0; $i < $lastNumber; $i++) {
-  $data[] = $name . ' ' . $i;
-}
-
-// Mengatur header sebagai JSON
 header('Content-Type: application/json');
 
-// Mengembalikan data dalam format JSON
-echo json_encode($data);
+$number = $_POST['number'];
+$text = $_POST['text'];
+
+$response = array();
+
+for ($i = 0; $i < $number; $i++) {
+  $response[] = $text . ' ' . ($i + 1);
+}
+
+echo json_encode($response);
 ?>
